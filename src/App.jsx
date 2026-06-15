188d8fcd1de120a4033ac334d1d8134114ba46ad
Oscarv2005 <oscarvishnoi0@gmail.com>
2026-06-15 19:44:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Result from "./Result";
 import Report from "./Report";
 import Login from "./login";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = "https://stude-back-ja3p.vercel.app";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
