72ee012b4384b93ed5ecd9f1fb10dfa0cbd95c68
Oscarv2005 <oscarvishnoi0@gmail.com>
2026-06-15 19:45:10 +0530

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = "https://stude-back-ja3p.vercel.app";
 
 function Login({ onAuthSuccess }) {
   const [isRegistering, setIsRegistering] = useState(false);
